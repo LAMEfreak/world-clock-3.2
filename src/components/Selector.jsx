cf6766aa76a50106d28a9c2b26f2bb8b3b19882c
LAMEfreak <leeweirong@outlook.com>
2024-01-22 19:44:21 +0800

    Fixed bugs

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,4 +1,4 @@
-import { Form } from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import sortedClockData from "../clock-data";
 import { useState } from "react";
